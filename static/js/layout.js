be0a0c6e13b2d2f44481d5d9ab119d8901862853
Serhii Zautkin <szautkin@gmail.com>
2024-12-13 15:27:28 -0800

    Updated file links, styles, js

--- static/js/layout.js
+++
@@ -1,8 +0,0 @@
-function toggleMobileMenu() {
-    const menu = document.getElementById('mobile-menu');
-    const button = document.querySelector('[aria-controls="mobile-menu"]');
-    const isExpanded = button.getAttribute('aria-expanded') === 'true';
-
-    menu.classList.toggle('hidden');
-    button.setAttribute('aria-expanded', !isExpanded);
-}
